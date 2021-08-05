77182d239a326ddf15dfb07b34872437fb7f9862
NonthawatNK <66126171+NonthawatNK@users.noreply.github.com>
2021-08-05 13:08:13 +0700

    Update to fix Button

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 const author = document.querySelector("#author")
 const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
+var toggle = false
+var authorText = author.innerText
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-const btn_search =document.querySelector("#search")
-const btn_reset = document.querySelector("#reset")
-const btn_display = document.querySelector("#display")
-
-// define more constants and variables here
+const search_btn = document.querySelector("#search");
+const reset_btn = document.querySelector("#reset");
+const text = document.querySelector('#text')
+var textData = text.innerText;
 
 btn_toggle.onclick = () => {
-  // your code here
+  if (!toggle) {
+    var l = length.value
+    var code = parseInt(author.innerText.split(" ")[0])
+    console.log(code - l)
+    author.innerText = code - l
+    btn_toggle.innerText = "Display Author"
+    toggle = true
+  }
+  else {
+    author.innerText = authorText
+    btn_toggle.innerText = "Display Calculation"
+    toggle = false
+  }
+
 }
-
-
-
-btn_search.onclick = () =>{
-
+search_btn.onclick = () => {
+  text.innerText = textData
+  var l = length.value
+  var words = text.innerText.split(" ")
+  var selectword = []
+  var color = document.querySelector("#color").value
+  for (const word of words) {
+    if (word.length >= l)
+      selectword.push(word)
+  }
+  for (const word of selectword) {
+    var innerHTML = text.innerHTML
+    var index = innerHTML.indexOf(word)
+    innerHTML = innerHTML.substring(0, index) + "<span style='background-color:" + color + " ;'>" + innerHTML.substring(index, index + word.length) + "</span>" + innerHTML.substring(index + word.length);
+    text.innerHTML = innerHTML;
+  }
 }
-
-
-btn_reset.onclick = () =>{
-
+reset_btn.onclick = () => {
+  location.reload()
 }
-
-btn_display.onclick = () =>{
-  
-}
-
